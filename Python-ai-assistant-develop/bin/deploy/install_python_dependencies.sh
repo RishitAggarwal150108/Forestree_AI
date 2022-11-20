62fac9d3d64241dc01d8cbb8dc5a763818110e2e
chintanck7 <67537038+chintanck7@users.noreply.github.com>
2022-11-21 00:29:40 +0530

    Delete Python-ai-assistant-develop/bin directory

--- Python-ai-assistant-develop/bin/deploy/install_python_dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#-----------------------------------
-# Install Python dependencies
-#-----------------------------------
-pip install -r requirements.txt
-pip install -U scikit-learn
